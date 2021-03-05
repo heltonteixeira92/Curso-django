ae369017d8f45a41c23c975f9b0e82dc1873ed63
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2021-03-05 01:04:54 -0300

    Collectfast e integração com entrega continua

--- pypro/settings.py
+++ pypro/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'collectfast',
     'django.contrib.staticfiles',
     'pypro.base',
 ]
@@ -134,6 +135,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
+COLLECTFAST_ENABLED = False
+
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 
 # STORAGE CONFIGURATION IN S3 AWS
@@ -148,7 +151,10 @@ if AWS_ACCESS_KEY_ID:
     AWS_AUTO_CREATE_BUCKET = False
     AWS_QUERYSTRING_AUTH = True
     AWS_S3_CUSTOM_DOMAIN = None
-    AWS_DEFAULT_ACL = 'private'
+
+    COLLECTFAST_ENABLED = True
+
+    AWS_DEFAULT_ACL = None
 
     # static assets
     STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
