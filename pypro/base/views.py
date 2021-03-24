dfd5d8f8f5e134b1395ba7d127fe2c8762fbfbea
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2021-03-24 12:33:02 -0300

    Instalando arquivos estáticos localmente e criando o home.html

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render # noqa
+from django.shortcuts import render
 
 # Create your views here.
 # a view é responsável por responder as requisições que fazemos pelo navegador
 
 
 def home(request):
-    return HttpResponse('<html><body>Olá Django</body></html>', content_type='text/html')
+    return render(request, 'base/home.html')
