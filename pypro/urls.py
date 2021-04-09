f66cf3ff034e673dd3f62b5bd67079286f94d18c
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2021-04-09 15:27:55 -0300

    Encapsulando Urls em Apps

--- pypro/urls.py
+++ pypro/urls.py
@@ -17,11 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 
-from pypro.base.views import home
+from pypro.base.views import home # noqa
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
+    path('', include('pypro.base.urls')),
 ]
 
 if settings.DEBUG:
