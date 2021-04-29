d292271d80ae46b596644e4430946b4963cc4c05
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2021-04-29 12:44:26 -0300

    incorporado primeiro video aperitivo e teste

--- pypro/aperitivos/tests.py
+++ pypro/aperitivos/tests.py
@@ -1,5 +1,5 @@
 from django.urls import reverse
-
+from pypro.django_assertions import assert_contains
 import pytest as pytest
 
 
@@ -10,3 +10,14 @@ def resp(client):
 
 def test_status_code(resp):
     assert resp.status_code == 200
+
+
+def test_titulo_video(resp):
+    assert_contains(resp, '<h1>Video Aperitivo: Motivação</h1>')
+
+
+def test_conteudo_video(resp):
+    assert_contains(resp, '<div style="padding:56.21% 0 0 0;position:relative;">'
+                          '<iframe src="https://player.vimeo.com/video/542810699?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" '
+                          'frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="aula-01-motivacao-curso-de-python-gratis.mp4"></iframe>'
+                          '</div><script src="https://player.vimeo.com/api/player.js"></script>')
