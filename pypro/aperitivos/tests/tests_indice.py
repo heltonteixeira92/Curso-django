3d4ac3829486697fb3c33b5744abbe2e8b70da3e
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2021-05-14 00:12:14 -0300

    Acesso a Método no temple e a criação do metodo get_absolute_url

--- pypro/aperitivos/tests/tests_indice.py
+++ pypro/aperitivos/tests/tests_indice.py
@@ -22,3 +22,16 @@ def test_status_code(resp):
 )
 def test_titulo_indice(resp, titulo):
     assert_contains(resp, titulo)
+
+
+@pytest.mark.parametrize(
+    'slug',
+    [
+        'motivacao',
+        'instalacao-windows',
+
+    ]
+)
+def test_link_indice(resp, slug):
+    video_link = reverse('aperitivos:video', args=(slug,))
+    assert_contains(resp, f'href="{video_link}"')
--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-
+from django.urls import reverse
 class Video:
     def __init__(self, slug, titulo, vimeo_id):
         self.slug = slug
         self.titulo = titulo
         self.vimeo_id = vimeo_id
 
+    def get_absolute_url(self):
+        return reverse('aperitivos:video', args=(self.slug,))
 
 videos = [
     Video('motivacao', 'Video Aperitivo: Motivação', 542810699),
