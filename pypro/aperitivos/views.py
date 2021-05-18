7839a776755144754ef95595c9f95443b43bc0a5
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2021-05-18 16:26:26 -0300

    Composição de Templates

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse
+
+
 class Video:
     def __init__(self, slug, titulo, vimeo_id):
         self.slug = slug
@@ -8,6 +10,7 @@ class Video:
 
     def get_absolute_url(self):
         return reverse('aperitivos:video', args=(self.slug,))
+
 
 videos = [
     Video('motivacao', 'Video Aperitivo: Motivação', 542810699),
