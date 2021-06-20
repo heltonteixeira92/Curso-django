f3792e145c03f5e64ac08816290b95816ae800ef
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2021-06-20 19:53:09 -0300

    consertado test_vimeo

--- pypro/modulos/tests/test_aula_detalhe.py
+++ pypro/modulos/tests/test_aula_detalhe.py
@@ -28,4 +28,7 @@ def test_titulo(resp, aula: Aula):
 
 
 def test_vimeo(resp, aula: Aula):
-    assert_contains(resp, f'src="https://player.vimeo.com/video/{ aula.vimeo_id }"')
+    assert_contains(resp, '<div style="padding:56.21% 0 0 0;position:relative;">'
+                          f'<iframe src="https://player.vimeo.com/video/{aula.vimeo_id}?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" '
+                          'frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="aula-01-motivacao-curso-de-python-gratis.mp4"></iframe>'
+                          '</div><script src="https://player.vimeo.com/api/player.js"></script>')
