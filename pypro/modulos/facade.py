67b0cafb359d63f44b6dba7ddc1f89383593411d
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2021-07-30 14:51:14 -0300

    usado metodo prefetch_related para abordado problemas do N+1 Selects

--- pypro/modulos/facade.py
+++ pypro/modulos/facade.py
@@ -1,5 +1,6 @@
 from typing import List
 from pypro.modulos.models import Modulo, Aula
+from django.db.models import Prefetch
 
 
 def listar_modulos_ordenados() -> List[Modulo]:
@@ -20,3 +21,10 @@ def listar_aulas_de_modulo_ordenadas(modulo: Modulo):
 
 def encontrar_aula(slug):
     return Aula.objects.select_related('modulo').get(slug=slug)
+
+
+def listar_modulos_com_aulas():
+    aulas_ordenadas = Aula.objects.order_by('order')
+    return Modulo.objects.order_by('order').prefetch_related(
+        Prefetch('aula_set', queryset=aulas_ordenadas, to_attr='aulas')
+                                                             ).all()
